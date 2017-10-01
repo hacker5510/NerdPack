e3ec47f4b15e63efef8bb76421297a4879784bf9
Jaime Marques <mrthesoulz@gmail.com>
2017-10-01 17:24:03 +0100

    we dont have glyphs anymore, deprecate it

--- deprecated.lua
+++ deprecated.lua
@@ -1,3 +1,24 @@
 local _, NeP = ...
 
 NeP.Globals.OM.GetRoster = function() return NeP.OM:Get("Roster") end
+
+NeP.DSL:Register("glyph", function(_,spell)
+  local spellId = tonumber(spell)
+  local glyphName, glyphId
+  for i = 1, 6 do
+    glyphId = select(4, _G.GetGlyphSocketInfo(i))
+    if glyphId then
+      if spellId then
+        if select(4, _G.GetGlyphSocketInfo(i)) == spellId then
+          return true
+        end
+      else
+        glyphName = NeP.Core:GetSpellName(glyphId)
+        if glyphName:find(spell) then
+          return true
+        end
+      end
+    end
+  end
+  return false
+end)
