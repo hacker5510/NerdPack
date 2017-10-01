732ccbf2d2c5f0e41649c1d35cc1a3e63b122264
Jaime Marques <mrthesoulz@gmail.com>
2017-10-01 17:24:24 +0100

    glyph

--- conditions/spell.lua
+++ conditions/spell.lua
@@ -102,24 +102,3 @@ end)
 NeP.DSL:Register("enchant.offhand", function()
   return (select(4, _G.GetWeaponEnchantInfo()) == 1)
 end)
-
-NeP.DSL:Register("glyph", function(_,spell)
-  local spellId = tonumber(spell)
-  local glyphName, glyphId
-  for i = 1, 6 do
-    glyphId = select(4, _G.GetGlyphSocketInfo(i))
-    if glyphId then
-      if spellId then
-        if select(4, _G.GetGlyphSocketInfo(i)) == spellId then
-          return true
-        end
-      else
-        glyphName = NeP.Core:GetSpellName(glyphId)
-        if glyphName:find(spell) then
-          return true
-        end
-      end
-    end
-  end
-  return false
-end)
